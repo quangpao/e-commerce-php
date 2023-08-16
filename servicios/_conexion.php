ae7485f2d119af71320f7e5c834737ae54c671f8
quangpao <ddquangbao@gmail.com>
2023-08-16 13:40:28 +0700

    change db host

--- servicios/_conexion.php
+++ servicios/_conexion.php
@@ -1,5 +1,5 @@
 <?php
-    $host = '10.0.0.5:3306'; // El nombre del servicio de la base de datos en el archivo docker-compose.yml; // El puerto mapeado en el archivo docker-compose.yml
+    $host = '10.0.128.5:3306'; // El nombre del servicio de la base de datos en el archivo docker-compose.yml; // El puerto mapeado en el archivo docker-compose.yml
     $database = 'sistema_ecommerce'; // El nombre de la base de datos definido en el archivo docker-compose.yml
     $user = 'hemish'; // El usuario definido en el archivo docker-compose.yml
     $password = 'contrasena123'; // La contraseña definida en el archivo docker-compose.yml
